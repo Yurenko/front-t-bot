e8bc000edb3ec9994e3790d5e9c192556b6d41f2
Yura Didenko <yurii.didenko@wtt-solutions.com>
2025-08-08 09:49:10 +0300

    Fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://back-t-bot.onrender.com"
+    ? "https://back-t-bot-k8mh.onrender.com"
     : process.env.REACT_APP_API_URL || "http://localhost:3007";
 
 const api = axios.create({
